b1afeac3cb0fc2fb7e03cbca73f89a40ca65288f
SBG_DESKTOP <gba2006su06@gmail.com>
2026-03-29 16:14:33 +0800

    no message

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,10 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
+      // 快捷鍵功能，用@符號直接指向src資料夾
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  // 部署路徑
+  base: process.env.NODE_ENV === 'production' ? '/vite-myportfolio/' : '/',
 })
